bbd718c337b73d8a1d9c6f3bebf32b902b28eb8c
penolove <chinghua.yang@appier.com>
2018-12-12 22:55:50 +0800

    add line_false_alert handler

--- monitor_reporter/line_feedback.py
+++ monitor_reporter/line_feedback.py
@@ -1,6 +1,11 @@
+import re
+
 import arrow
 import peewee
 from eyewitness.audience_id import AudienceId
+from eyewitness.config import FEEDBACK_FALSE_ALERT, LINE_FALSE_ALERT_ANNOTATION_PATTERN
+from eyewitness.feedback_msg_utils import FeedbackMsg
+from eyewitness.image_id import ImageId
 from flask import Blueprint, request, current_app, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextSendMessage, TextMessage
@@ -14,7 +19,7 @@ LINE_CHANNEL_SECRET = ''
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 bp = Blueprint('line_feedback', __name__, url_prefix='/line_feedback')
-# annotation_pattern = re.compile("(\d+)\/(\d+)\/(\d+)\/(\d+)\/.*.jpg")
+FALSE_ALERT_ANNOTATION_PATTERN = re.compile(LINE_FALSE_ALERT_ANNOTATION_PATTERN)
 
 
 # TODO webhook for annotation
@@ -40,10 +45,9 @@ def handle_text_message(event):
     false_alter_feedback_handler = current_app.config['FALSE_ALTER_FEEDBACK_HANDLER']
     content = event.message.text
     line_id = event.source.user_id
-    arrive_timestamp = arrow.now().datetime
+    audience_id = AudienceId(user_id=line_id, platform_id='line')
+    arrive_timestamp = arrow.now().timestamp
     if 'open the door' in content:
-        # TODO register the audience
-        audience_id = AudienceId(user_id=line_id, platform_id='line')
         try:
             false_alter_feedback_handler.register_audience(
                 audience_id, {'register_time': arrive_timestamp})
@@ -57,8 +61,28 @@ def handle_text_message(event):
                 TextSendMessage(text="maybe you already registered")
             )
     else:
-        # TODO pattern check and create feedback result
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content)
-        )
+        matched_obj = FALSE_ALERT_ANNOTATION_PATTERN.match(content)
+        # if false_alert feedback pattern matched
+        if matched_obj is not None:
+            false_alter_feedback_handler = current_app.config['FALSE_ALTER_FEEDBACK_HANDLER']
+            feedback_dict = matched_obj.groupdict()
+            image_id = ImageId(
+                feedback_dict['channel'], feedback_dict['timestamp'], feedback_dict['file_format'])
+            feedback_dict = {
+                'audience_id': audience_id, 'receive_time': arrive_timestamp,
+                'feedback_method': FEEDBACK_FALSE_ALERT, 'image_id': image_id,
+                'feedback_meta': ''
+            }
+            # generate FeebackMsg obj
+            feedback_obj = FeedbackMsg(feedback_dict)
+            # insert into database
+            false_alter_feedback_handler.handle(feedback_obj)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="annotation received: %s" % (str(image_id)))
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="No matched pattern")
+            )
