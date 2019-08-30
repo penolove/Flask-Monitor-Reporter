6844218ce00010abc2a947ee062ddcff2112bb94
penolove <chinghua.yang@appier.com>
2019-08-31 00:57:33 +0800

    bump album implementation

--- monitor_reporter/album.py
+++ monitor_reporter/album.py
@@ -11,16 +11,14 @@ def display_obj_drawn_images(target, year, month, day, hour):
     start_time = datetime(year, month, day, hour)
     end_time = start_time + timedelta(hours=1)
     drawn_images = set()
-    channels = set(i.channel for i in ImageInfo.select())
 
-    for channel in channels:
-        query = BboxDetectionResult.select().join(ImageInfo).where(
-            BboxDetectionResult.label == target,
-            ImageInfo.channel == channel,
-            ImageInfo.timestamp >= start_time,
-            ImageInfo.timestamp <= end_time,)
-        drawn_images |= set(
-            i.image_id.drawn_image_path for i in query if i.image_id.drawn_image_path)
+    query = BboxDetectionResult.select(BboxDetectionResult, ImageInfo).join(ImageInfo).where(
+        BboxDetectionResult.label == target,
+        ImageInfo.timestamp >= start_time,
+        ImageInfo.timestamp <= end_time,)
+
+    drawn_images |= set(
+        i.image_id.drawn_image_path for i in query if i.image_id.drawn_image_path)
 
     img_list = [url_for('static', filename=i) for i in sorted(drawn_images)]
     return render_template('album.html', img_list=img_list, image_amount=len(img_list))
